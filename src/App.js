da72b433a2fbe6f3e297a5a176acbea3354765ff
Keith <kqbui@csu.fullerton.edu>
2025-03-25 17:40:36 -0700

    🔨 updated React side

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import BookHandler from './typing_prompt.js';
 
 function App() {
   return (
-    <div className="App">
-        <script src="typing_prompt.js"></script>
-        <div class="App" id="UI">
-          <div class="title">TypeFighter</div>
-          <div class="subtitle">Will the boss fall today?</div>
-          <div class="hp-bar">HP: 100%</div>
-          <button className="attack-button" onClick={() => BookHandler.retrieve_book_data('OL7353617M')}>Attack!</button>
-        </div>
-    </div>
+    <div className="App" id="UI">
+        <div className="title">TypeFighter</div>
+        <div className="subtitle">Will the boss fall today?</div>
+        <img className="boss" src="/1200px-Bowser.webp" alt="Bowser" />
+        <div className="hp-bar">HP: 100%</div>
+        <button
+          className="attack-button"
+          onClick={() => BookHandler.retrieve_book_data('OL7353617M')}
+        >
+          Attack!
+        </button>
+      </div>
   );
 }
+
 export default App;
+
